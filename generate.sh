a463b33bd1e8226ba70d3b38396e7197ea4f1e5a
LLLLLA7 <24212020123@m.fudan.edu.cn>
2024-10-30 14:29:49 +0800

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,8 +1,9 @@
 python generate.py \
   --device 0 \
-  --length 900 \
+  --length 500 \
   --tokenizer_path cache/vocab_wanmei.txt \
   --model_path model_wanmei/final_model \
   --prefix "[CLS][MASK]" \
   --topp 1 \
   --temperature 1.0
+  --save_samples
